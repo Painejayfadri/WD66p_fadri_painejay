3ae5aeb78c695f219f2e30dce2593e4c4524ded7
Paine Jay Fadri <fadripainee@gmail.com>
2023-08-24 20:31:30 +0800

    My Capstone

---
+++ new capstone/main.js
@@ -0,0 +1,43 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick =() =>{
+  search.classList.toggle('active');
+  cart.classList.remove('active');
+  user.classList.remove('active');
+}
+
+let cart = document.querySelector('.cart');
+
+document.querySelector('#cart-icon').onclick =() =>{
+  cart.classList.toggle('active');
+  search.classList.remove('active');
+  user.classList.remove('active');
+}
+
+let user = document.querySelector('.user');
+
+document.querySelector('#user-icon').onclick =() =>{
+  user.classList.toggle('active');
+  cart.classList.remove('active');
+  search.classList.remove('active');
+}
+
+
+// Swiper
+
+var swiper = new Swiper(".new-arrival", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+});
